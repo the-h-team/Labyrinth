76d33e5c5b1b1fc6e8601ad8d3659df0d3bf42aa
Hempfest <64885225+Hempfest@users.noreply.github.com>
2021-03-30 20:39:03 -0700

    * Made desired changes to Message.java

--- src/main/java/com/github/sanctum/labyrinth/library/Message.java
+++ src/main/java/com/github/sanctum/labyrinth/library/Message.java
@@ -6,17 +6,19 @@ import net.md_5.bungee.api.chat.BaseComponent;
 import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
 
 public class Message {
 
 	private final Logger logger = Logger.getLogger("Minecraft");
-	private final String prefix;
+	private String prefix;
 	private Player p;
 
 
 	/**
 	 * NOTE: Only for console use
 	 * Send easy messages through console with prefix specification.
+	 *
 	 * @param prefix The prefix to be used for console.
 	 */
 	public Message(String prefix) {
@@ -25,6 +27,7 @@ public class Message {
 
 	/**
 	 * Update the player to recieve the messages.
+	 *
 	 * @param player The player to now recieve messages.
 	 */
 	public void assignPlayer(Player player) {
@@ -32,7 +35,17 @@ public class Message {
 	}
 
 	/**
+	 * Update the prefix used for the message.
+	 *
+	 * @param prefix The prefix to use.
+	 */
+	public void setPrefix(String prefix) {
+		this.prefix = prefix;
+	}
+
+	/**
 	 * Send a specified player messages.
+	 *
 	 * @param p The player to use.
 	 */
 	public Message(Player p) {
@@ -42,6 +55,7 @@ public class Message {
 
 	/**
 	 * Send a specified player messages with a specified prefix.
+	 *
 	 * @param p The player to use.
 	 * @param prefix The prefix to send.
 	 */
@@ -52,6 +66,7 @@ public class Message {
 
 	/**
 	 * Send a string message to a player automatically colored.
+	 *
 	 * @param text The context to send the player
 	 */
 	public void send(String text) {
@@ -74,14 +89,16 @@ public class Message {
 
 	/**
 	 * Send the player an interactive chat component.
+	 *
 	 * @param component The component to build and send.
 	 */
-	public void build(TextComponent component) {
+	public void build(BaseComponent component) {
 		p.spigot().sendMessage(component);
 	}
 
 	/**
 	 * Send a player the list of interactive chat components.
+	 *
 	 * @param components The list of interactive chat to send.
 	 */
 	public void build(BaseComponent... components) {
@@ -91,6 +108,7 @@ public class Message {
 	/**
 	 * NOTE: Only for console use.
 	 * Send console an info message.
+	 *
 	 * @param text The text to be used within the message
 	 */
 	public void info(String text) {
@@ -100,6 +118,7 @@ public class Message {
 	/**
 	 * NOTE: Only for console use.
 	 * Send console an error message.
+	 *
 	 * @param text The text to be used within the message
 	 */
 	public void error(String text) {
@@ -109,10 +128,31 @@ public class Message {
 	/**
 	 * NOTE: Only for console use.
 	 * Send console a warning message.
+	 *
 	 * @param text The text to be used within the message
 	 */
 	public void warn(String text) {
 		logger.severe(String.format("[%s] - " + text, prefix));
+	}
+
+	/**
+	 * Use this to easily send message degrees to console.
+	 *
+	 * @param plugin The plugin to log console messages for.
+	 * @return A console messagiwng object.
+	 */
+	public static Message loggedFor(Plugin plugin) {
+		return new Message(plugin.getName());
+	}
+
+	/**
+	 * Use this to easily send message's to player or console
+	 *
+	 * @param p The target.
+	 * @return A player & console messaging object.
+	 */
+	public static Message form(Player p) {
+		return new Message(p);
 	}
 
 
