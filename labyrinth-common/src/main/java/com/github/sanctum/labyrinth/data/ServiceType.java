5eba4cc2d4af7f80c1ae395ff4bf7c0116c1d1fc
rigob <ge35sot@mytum.de>
2021-09-01 14:00:52 +0200

    ServiceType Constructor Runtime Enhancement. Now no longer uses the expensive operation of building an instance to get the Service class

--- labyrinth-common/src/main/java/com/github/sanctum/labyrinth/data/ServiceType.java
+++ labyrinth-common/src/main/java/com/github/sanctum/labyrinth/data/ServiceType.java
@@ -1,6 +1,8 @@
 package com.github.sanctum.labyrinth.data;
 
 import com.github.sanctum.labyrinth.api.Service;
+
+import java.util.Arrays;
 import java.util.function.Supplier;
 
 public class ServiceType<T extends Service> {
@@ -8,9 +10,12 @@ public class ServiceType<T extends Service> {
 	final Supplier<T> supplier;
 	Class<T> c;
 
+	@SuppressWarnings("unchecked")
 	public ServiceType(Supplier<T> supplier) {
 		this.supplier = supplier;
-		c = (Class<T>) supplier.get().getClass();
+		c = (Class<T>) Arrays.stream(supplier.getClass().getMethods())
+				.filter(m -> m.getName().equals("get") && m.getParameterTypes().length == 0)
+				.findFirst().orElseThrow((IllegalArgumentException::new)).getReturnType();
 	}
 
 	public Class<T> getType() {
